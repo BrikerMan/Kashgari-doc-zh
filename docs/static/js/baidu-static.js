c14662881f9f3421578522562ee06e1e2f341d7e
Eliyar Eziz <eliyar917@gmail.com>
2019-07-14 10:18:11 +0800

    :tada: init docs

---
+++ docs/static/js/baidu-static.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f9e1a75f1e90250ed4b49ac0c804685b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
